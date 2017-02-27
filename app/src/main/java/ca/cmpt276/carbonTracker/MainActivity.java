8aa31387dbd7a7b9d2be65e2bffdf8ac9cc2dcdf
Alex Elesin <aelesin@sfu.ca>
2017-02-27 01:06:34 -0800

    Initial Project Build

---
+++ app/src/main/java/ca/cmpt276/carbonTracker/MainActivity.java
@@ -0,0 +1,15 @@
+package ca.cmpt276.carbonTracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sasha.carbontracker.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
