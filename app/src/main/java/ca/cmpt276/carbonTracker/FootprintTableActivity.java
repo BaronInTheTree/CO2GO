81000355abb1054a4af288364cf9274ab9401a58
Song Xiao <sxa17@sfu.ca>
2017-02-28 11:09:06 -0800

    Remove the csv file from res/docs; implement FootprintTableActivity with testing data.
    Remove the csv file temporarily to allow testing other parts of the app.

--- app/src/main/java/ca/cmpt276/carbonTracker/FootprintTableActivity.java
+++ app/src/main/java/ca/cmpt276/carbonTracker/FootprintTableActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.sasha.carbontracker.R;
 
@@ -15,6 +17,12 @@ public class FootprintTableActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_footprint_table);
 
+        populateListView();
+
+        setUpButtons();
+    }
+
+    private void setUpButtons() {
         Button toGraph_btn = (Button) findViewById(R.id.toGraph_btn);
         toGraph_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,5 +30,24 @@ public class FootprintTableActivity extends AppCompatActivity {
                 startActivity(new Intent(FootprintTableActivity.this, FootprintGraphActivity.class));
             }
         });
+
+        Button back_btn = (Button) findViewById(R.id.back_btn);
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+
+    private void populateListView() {
+        String[] allJourneys = {"Route 1      1000km        myVehicle      589.75"
+                , "Route 2      200.34km     myVehicle      123.75"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.table_layout,allJourneys);
+        ListView list = (ListView)findViewById(R.id.tableFP);
+        list.setAdapter(adapter);
+
     }
 }
