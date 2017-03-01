28fa52e8bfa27be84542aa8d1ec01998c65c5985
Song Xiao <sxa17@sfu.ca>
2017-02-28 16:09:18 -0800

    Initial implementation of FootprintGraph Activity with testing data.

--- app/src/main/java/ca/cmpt276/carbonTracker/FootprintGraphActivity.java
+++ app/src/main/java/ca/cmpt276/carbonTracker/FootprintGraphActivity.java
@@ -4,12 +4,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.sasha.carbontracker.R;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FootprintGraphActivity extends AppCompatActivity {
+
+    double emissions[] = {23.4,5.8,56.12,189.46,70.77,23.5,94.23};
+    String journeys[]={"#1", "#2", "#3", "#4", "#5", "#6","#7"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_footprint_graph);
+
+        setupPieChart();
+
+    }
+
+    private void setupPieChart() {
+        List<PieEntry> pieEntries = new ArrayList<>();
+        for (int i=0;i<emissions.length;i++){
+            pieEntries.add(new PieEntry((float) emissions[i],journeys[i]));
+        }
+
+        PieDataSet dataSet = new PieDataSet(pieEntries,"Emission of Each Journey");
+        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+        PieData data = new PieData(dataSet);
+
+        PieChart chart = (PieChart) findViewById(R.id.chart);
+        chart.setData(data);
+        chart.invalidate();
+
     }
 }
