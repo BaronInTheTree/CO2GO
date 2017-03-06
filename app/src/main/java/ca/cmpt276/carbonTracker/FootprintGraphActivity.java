773da7a462397c35e4ba50f297feba0eb5e54974
Song Xiao <sxa17@sfu.ca>
2017-03-06 14:45:51 -0800

    update comments and the layout of journey Info activity.

--- app/src/main/java/ca/cmpt276/carbonTracker/FootprintGraphActivity.java
+++ app/src/main/java/ca/cmpt276/carbonTracker/FootprintGraphActivity.java
@@ -25,11 +25,12 @@ public class FootprintGraphActivity extends AppCompatActivity {
 
     private final String tableLabel = "CO2 Emission of Journeys (in gram)";
 
-    // String journeys[] = getJourneyDescription();
+    // CarbonModel currentInstance = CarbonModel.getInstance();
+    // String journeys[] = currentInstance.getJourneys().getJourneyDescription();
     String journeys[] = {"#1: 2017-01-01, Home_to_Work_Route, 1234567890, my_Vehicle_Informa, 12345678","#2:date, routeName...","#3:date, routeName...",
             "#4:date, routeName...", "#5:date, routeName...","#6:date, routeName...", "#7:date, routeName..."};
 
-    // int emissions[] = getJourneyEmission();
+    // int emissions[] = currentInstance.getJourneys().getJourneyEmission();
     int emissions[] = {23,5,56,189,70,23,94};
 
     @Override
