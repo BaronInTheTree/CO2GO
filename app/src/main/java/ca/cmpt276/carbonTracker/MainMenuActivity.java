21bd1367326515cd1bcb630d77c8325fe96c5f6b
Alex Elesin <aelesin@sfu.ca>
2017-03-06 13:21:14 -0800

    Reads csv file in new thread
    Moved the process of reading in from file into welcome activity from main.
    Cleaned up both classes

--- app/src/main/java/ca/cmpt276/carbonTracker/MainMenuActivity.java
+++ app/src/main/java/ca/cmpt276/carbonTracker/MainMenuActivity.java
@@ -3,76 +3,24 @@ package ca.cmpt276.carbonTracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.sasha.carbontracker.R;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-
 public class MainMenuActivity extends AppCompatActivity {
-
-    CarbonModel modelInstance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-        modelInstance = CarbonModel.getInstance();
-
-        readVehicleDate();
 
         newJourneyButton();
 
         footPrintButton();
-
     }
 
-    public void readVehicleDate() {
-        InputStream is = getResources().openRawResource(R.raw.vehiclesnew); // CHANGED
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(is, Charset.forName("UTF-8"))
-        );
-
-        String line = "";
-        try {
-            // Step over headers
-            line = reader.readLine();
-
-            // Read in file until empty
-            while ((line = reader.readLine()) != null) {
-                // Split by ','
-                String[] tokens = line.split(",");
-
-                // Read and store data
-                String make = tokens[0];
-                String model = tokens[1];
-                int year = Integer.parseInt(tokens[2]);
-                int highwayMPG = Integer.parseInt(tokens[3]);
-                int cityMPG = Integer.parseInt(tokens[4]);
-                String trany = tokens[5];
-                int cylinders = Integer.parseInt(tokens[6]);
-                double displacement = Double.parseDouble(tokens[7]);
-                String fuelType = tokens[8];
-                Car car = new Car(make, model, year, highwayMPG, cityMPG, trany, cylinders, displacement, fuelType);
-                modelInstance.getCarData().getCarDataList().add(car);
-            }
-        } catch (IOException e) {
-            Log.i("MyActivity", "Error reading data from file on line" + line, e);
-            e.printStackTrace();
-        }
-        modelInstance.getCarData().initializeMakeList();
-        modelInstance.getCarData().updateModelList("Toyota");
-        modelInstance.getCarData().updateYearList("Toyota", "Yaris");
-        System.out.println("carList size: " + modelInstance.getCarData().getCarDataList().size());
-    }
-
-    public void newJourneyButton() {
+    private void newJourneyButton() {
         Button journey_btn = (Button) findViewById(R.id.newJourney_btn);
         journey_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,7 +30,7 @@ public class MainMenuActivity extends AppCompatActivity {
         });
     }
 
-    public void footPrintButton() {
+    private void footPrintButton() {
         Button footprint_btn = (Button) findViewById(R.id.footprint_btn);
         footprint_btn.setOnClickListener(new View.OnClickListener() {
             @Override
