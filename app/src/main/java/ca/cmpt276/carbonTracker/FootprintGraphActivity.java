b1a3bcea21e628ed86bf465aee7aba099e85d4c3
Alex Elesin <aelesin@sfu.ca>
2017-03-06 16:16:38 -0800

    Additional comments of Graph and JourneyInfoActivity

--- app/src/main/java/ca/cmpt276/carbonTracker/FootprintGraphActivity.java
+++ app/src/main/java/ca/cmpt276/carbonTracker/FootprintGraphActivity.java
@@ -3,7 +3,6 @@ package ca.cmpt276.carbonTracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -27,8 +26,8 @@ public class FootprintGraphActivity extends AppCompatActivity {
 
     // CarbonModel currentInstance = CarbonModel.getInstance();
     // String journeys[] = currentInstance.getJourneys().getJourneyDescription();
-    String journeys[] = {"#1: 2017-01-01, Home_to_Work_Route, 1234567890, my_Vehicle_Informa, 12345678","#2:date, routeName...","#3:date, routeName...",
-            "#4:date, routeName...", "#5:date, routeName...","#6:date, routeName...", "#7:date, routeName..."};
+    String journeys[] = {"#1: 2017-01-01, Home_to_Work_Route, 1234567890, my_Vehicle_Informa, 12345678", "#2:date, routeName...", "#3:date, routeName...",
+            "#4:date, routeName...", "#5:date, routeName...", "#6:date, routeName...", "#7:date, routeName..."};
 
     // int emissions[] = currentInstance.getJourneys().getJourneyEmission();
     int emissions[] = {23, 5, 56, 189, 70, 23, 94};
@@ -76,7 +75,6 @@ public class FootprintGraphActivity extends AppCompatActivity {
             public void onNothingSelected() {
             }
         });
-
         chart.invalidate();
     }
 
