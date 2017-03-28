37d4a56c0cecae1e75741c17f4cbcad9e405df0c
Adriel <adrielr@sfu.ca>
2017-03-28 10:53:47 -0700

    Add conversion rate

--- app/src/main/java/ca/cmpt276/carbonTracker/Internal_Logic/TreeUnit.java
+++ app/src/main/java/ca/cmpt276/carbonTracker/Internal_Logic/TreeUnit.java
@@ -2,11 +2,14 @@ package ca.cmpt276.carbonTracker.Internal_Logic;
 
 /**
  * The TreeUnit class implements a human relatable unit for display when
- * a user views CO2 usage. The conversion rate is 1
+ * a user views CO2 usage. The conversion rate is 1 Tree = 22.06 grams of CO2.
+ *
+ * @author Team Teal
  */
 
 public class TreeUnit {
     private boolean isTreeEnabled;
+    private static final double TREE_TO_CO2 = 22.06;
 
     public TreeUnit (boolean isTreeEnabled) {
         this.isTreeEnabled = isTreeEnabled;
