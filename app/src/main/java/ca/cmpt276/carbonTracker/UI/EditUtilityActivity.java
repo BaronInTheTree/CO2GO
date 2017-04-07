e44a1f3eaf74ade185425233aac3a243c7a3e7b5
kyhenry <kyle_henry12@hotmail.com>
2017-04-07 15:05:39 -0700

    Update UtilitySummary and EditUtility UI

--- app/src/main/java/ca/cmpt276/carbonTracker/UI/EditUtilityActivity.java
+++ app/src/main/java/ca/cmpt276/carbonTracker/UI/EditUtilityActivity.java
@@ -4,16 +4,27 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ActionMenuView;
+import android.support.v7.widget.SwitchCompat;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.sasha.carbontracker.R;
 
+import ca.cmpt276.carbonTracker.AlternateUI.AddCarActivity;
+import ca.cmpt276.carbonTracker.AlternateUI.AddJourneyActivity_Alternate;
+import ca.cmpt276.carbonTracker.AlternateUI.AddRouteActivity;
+import ca.cmpt276.carbonTracker.AlternateUI.AddUtilityActivity;
+import ca.cmpt276.carbonTracker.AlternateUI.MainMenuActivity_Alternate;
 import ca.cmpt276.carbonTracker.Internal_Logic.*;
 
 /*
@@ -39,6 +50,7 @@ public class EditUtilityActivity extends AppCompatActivity {
     private int numPeople;
     int selectedUtilityIndex;
     private Utility selectedUtility;
+    private ActionMenuView amvMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,6 +84,7 @@ public class EditUtilityActivity extends AppCompatActivity {
         // Save and cancel buttons
         setupEditBillButton();
         setupCancelButton();
+        setupActionBar();
     }
 
     private void setupUtilityNicknameText() {
@@ -412,5 +425,95 @@ public class EditUtilityActivity extends AppCompatActivity {
 
     public static Intent makeIntent(Context context) {
         return new Intent(context, EditUtilityActivity.class);
+    }
+
+    private void setupActionBar() {
+        // Inflate your custom layout
+        Toolbar toolBar = (Toolbar) findViewById(R.id.mainMenu_toolBar);
+        amvMenu = (ActionMenuView) toolBar.findViewById(R.id.amvMenu);
+        amvMenu.setOnMenuItemClickListener(new ActionMenuView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return onOptionsItemSelected(menuItem);
+            }
+        });
+        setSupportActionBar(toolBar);
+        getSupportActionBar().setTitle(null);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_actions_mainmenu, amvMenu.getMenu());
+        MenuItem itemSwitch = amvMenu.getMenu().findItem(R.id.toolbar_switch);
+        itemSwitch.setActionView(R.layout.switch_layout);
+
+        final SwitchCompat unitToggle = (SwitchCompat) amvMenu.getMenu().findItem(R.id.toolbar_switch).
+                getActionView().findViewById(R.id.switchForActionBar);
+        if (CarbonModel.getInstance().getTreeUnit().getTreeUnitStatus()) {
+            unitToggle.setChecked(true);
+        }
+        unitToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    CarbonModel.getInstance().getTreeUnit().setTreeUnitStatus(true);
+                    SaveData.saveTreeUnit(EditUtilityActivity.this);
+                } else {
+                    CarbonModel.getInstance().getTreeUnit().setTreeUnitStatus(false);
+                    SaveData.saveTreeUnit(EditUtilityActivity.this);
+                }
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch(id) {
+            case R.id.action_settings: {
+                return true;
+            }
+            case R.id.action_addVehicle: {
+                Intent intent = new Intent(EditUtilityActivity.this, AddCarActivity.class);
+                intent.putExtra("Caller", "EditUtility");
+                startActivity(intent);
+                finish();
+                return true;
+            }
+            case R.id.action_addRoute: {
+                Intent intent = new Intent(EditUtilityActivity.this, AddRouteActivity.class);
+                intent.putExtra("Caller", "EditUtility");
+                startActivity(intent);
+                finish();
+                return true;
+            }
+            case R.id.action_addUtility: {
+                Intent intent = new Intent(EditUtilityActivity.this, AddUtilityActivity.class);
+                intent.putExtra("Caller", "EditUtility");
+                startActivity(intent);
+                finish();
+                return true;
+            }
+            case R.id.action_addJourney: {
+                Intent intent = new Intent(EditUtilityActivity.this, AddJourneyActivity_Alternate.class);
+                intent.putExtra("Caller", "EditUtility");
+                startActivity(intent);
+                finish();
+                return true;
+            }
+            case android.R.id.home: {
+                Intent intent = new Intent(EditUtilityActivity.this, MainMenuActivity_Alternate.class);
+                startActivity(intent);
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
